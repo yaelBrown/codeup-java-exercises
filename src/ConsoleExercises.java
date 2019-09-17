835a33bfe46d544a6a2a66339da09f2226a36b25
yaelBrown <yaelrbrown@gmail.com>
2019-09-17 12:27:23 -0500

    Prelunch push, finished Console Exercises.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -8,11 +8,78 @@ public class ConsoleExercises {
 
         System.out.format("The value of pi is approximately: %.2f%n", pi);
 
+        // multiple wildcards are separated by commas.
+        String name = "codeup";
+        String cohort = "Ceres";
+        System.out.format("Welcome, %s, nice to see you %s!\n", cohort, name);
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a integer");
 
-        String num = scanner.nextLine();
-//        Explore Scanner class #1
+        // You will get runtime error if you enter something other than a number.
+        // InputMismatchException
+        int numInt = scanner.nextInt();
+
+        System.out.println("Enter three words");
+        String wordOne = scanner.next();
+        String wordTwo = scanner.next();
+        String wordThr = scanner.next();
+
+        System.out.println(wordOne);
+        System.out.println(wordTwo);
+        System.out.println(wordThr);
+
+        // After last lines, add this to create a break to add more lines.
+        scanner.nextLine();
+
+        System.out.println("\nEnter a sentence");
+
+        String sentence = scanner.nextLine();
+
+        System.out.printf("You entered: %s", sentence);
+
+        System.out.println("\nCalculate Perimeter !");
+
+        System.out.println("\nEnter the length");
+        int length = scanner.nextInt();
+        // int length = Integer.parseInt(input.nextLine());
+
+        System.out.println("Enter the width");
+        int width = scanner.nextInt();
+
+        int perimeter = (length * 2) + (width * 2);
+
+        // parseInt
+        // valueOf
+
+        //Parsing values or getting the value of
+
+//        String piString = "3.1416";
+//        double pi = Double.valueOf(piString);
+//        double piParsed = Double.parseDouble(piString);
+
+        // Now that they are doubles we can do math with them
+
+//        pi = pi *2;
+//        piParsed = piParsed *3;
+//
+//        System.out.println("piParsed = " + pi);
+//        System.out.println("piParsed = " + piParsed);
+
+        System.out.printf("\nPerimeter of codeup's classrooms is %d feet.\n", perimeter);
+
+        System.out.println("Insert height to calculate volume");
+        int height = scanner.nextInt();
+        int volume = perimeter * height;
+
+        System.out.printf("Volume of the classroom is %d\n", volume);
+
+        System.out.println("Provide a decimal entry: ");
+        double decimalEntry = scanner.nextDouble();
+
+        System.out.printf("Decimal entry you provided is %.2f", decimalEntry);
+
+
 
     }
 }
