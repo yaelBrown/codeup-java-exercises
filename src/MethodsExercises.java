df7e03115aa04663e96818505f6acc6dde7ac3b8
yaelBrown <yaelrbrown@gmail.com>
2019-09-19 16:22:43 -0500

    Finished method exercises up to roll dice game.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class MethodsExercises {
+
+    public static void main(String[] args) {
+//        addition(2,3);
+//        subtraction(4,2);
+//        division(15,3);
+//        multiplication(5,5);
+//        modulus(15, 3);
+//
+//        getInput(1,10);
+
+//        printFactorial(10);
+    }
 
     // Addition
     public static int addition(int a, int b) {
@@ -37,7 +50,8 @@ public class MethodsExercises {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int temp = Integer.parseInt(scanner.nextLine());
-        if (temp > min && temp < max) {
+        if (temp >= min && temp <= max) {
+            System.out.println("This is a valid number");
             return temp;
         } else {
             System.out.println("Number out of range, try again!");
@@ -46,14 +60,30 @@ public class MethodsExercises {
         return temp;
     }
 
-    public static void main(String[] args) {
-        addition(2,3);
-        subtraction(4,2);
-        division(15,3);
-        multiplication(5,5);
-        modulus(15, 3);
+    // Calc and display factorial
+    public static void printFactorial(int num) {
+        String multiples = "";
+        int factorial = 0;
+        String iteratorToString;
 
-        getInput(1,10);
+        for (int i = 1; i <= num; i++) {
+            iteratorToString = Integer.toString(i);
+            if (i == 1) {
+                multiples = iteratorToString;
+                factorial = i;
+            }
+            if (i > 1) {
+                multiples += " x " + iteratorToString;
+                factorial = factorial * i;
+            }
+            System.out.printf("%4d!    %-60s = %d \n", i, multiples, factorial);
+        }
+
     }
+
+//    public static void rollDice() {
+//
+//    }
+
 
 }
