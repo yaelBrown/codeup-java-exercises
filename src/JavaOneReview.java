f7b126cd97bb46fb468e8bb969eb8b133e024e2f
yaelBrown <yaelrbrown@gmail.com>
2019-09-23 08:37:44 -0500

    Added Java One Review

---
+++ src/JavaOneReview.java
@@ -0,0 +1,90 @@
+public class JavaOneReview {
+
+    public static void main(String[] args) {
+
+        // ============================== Casting (especially numbers)
+
+    /*
+
+        Casting occurs when the type of a value becomes different than its original type.
+
+        Casting will often occur when working with numbers and Java inheritance.
+        We will see examples of Inheritance in Java II. Number types can cast to a larger
+        number type (e.g. short to a long) or to a smaller type (e.g. long to a short).
+        Going from larger number type to a smaller type is called "explicit" casting and
+        requires the programmer to write extra syntax to indicate that this potentially risky
+        operation is REALLY what we want to happen. Going the opposite direction is called
+        "implicit" casting and does not require extra syntax.
+
+        Helpful chart: https://www.w3schools.com/java/java_type_casting.asp
+
+        int x = 1;
+        long y = x; // implicit casting
+        short z = (short) x; // explicit casting
+
+     */
+
+
+        // ============================== List of helpful built-in methods
+
+    /*
+        The String and primitive wrapper class (Integer, Byte, Character, etc.) methods
+        The Scanner, Math class methods
+     */
+
+
+        // ============================== String manipulation practice
+
+    /*
+        - create your own examples of each of the curriculum Java string methods
+        - create variations of the String exercises and bonuses
+        - Google for more
+     */
+
+        // ============================== Switch statements (how and why and when)
+
+    /*
+        Use the Switch case ONLY when checking for multiple possible values for a single given variable.
+        In all other cases, use regular if/else if statements.
+     */
+
+        // ============================== List of resources for Java
+    /*
+        - Java source code in the java.util and java.lang packages
+        - Head First Java
+        - The Oracle Java Docs:
+            - https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/package-summary.html
+            - https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/package-summary.html
+        - StackOverFlow
+        - Additional Resources: https://www.baeldung.com/java-blogs
+        - Derek Banas on YouTube
+     */
+
+
+        // ============================== Working with imports (standard imports)
+
+    /*
+        most often from the classes and methods from the java.util package
+        Additional external libraries (we will learn about a tool to easily install these later in the course)...
+            Apache Commons https://commons.apache.org/
+            Guava https://github.com/google/guava/wiki
+    */
+
+        // ============================== Essential Vocabulary
+
+    /*
+        - create your own word bank (start from curriculum)
+        - expand your word bank with helpful tools and concepts for solving curriculum exercises
+    */
+
+        // ============================== Recursion (when and why)
+
+    /*
+
+        Use recursion when it makes your code more easily readable than using a loop.
+
+     */
+
+    }
+
+}
